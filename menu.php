7199a6a35416cf196e82f43ce14cdc5a9da8b845
Dominika <barbierikovadominika@gmail.com>
2020-10-20 17:28:53 +0200

    utorok

--- menu.php
+++ menu.php
@@ -1,5 +1,3 @@
-<?php
-?>
 <!DOCTYPE html>
 <html lang="sk">
 
@@ -15,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Tenor+Sans&amp;display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="uvodna_strana.css">
-</head
+</head>
 <body>
 
 <!--footer-->
@@ -27,23 +25,23 @@
 
 <!--menu-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top ">
-    <a class="navbar-brand" href="uvodna_strana.php">Piváreň u nás</a>
+    <a class="navbar-brand" href="uvodna_strana.html">Piváreň u nás</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="uvodna_strana.php">O nás</a>
+                <a class="nav-link" href="uvodna_strana.html">O nás</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="pohare.php">Poháre</a>
+                <a class="nav-link" href="pohare.html">Poháre</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Menu <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="Kontakt.php">Kontakt</a>
+                <a class="nav-link" href="Kontakt.html">Kontakt</a>
             </li>
         </ul>
     </div>
