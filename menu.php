366f4b02882eb7fc322f4af7f15678ee3c5181ee
Dominika <barbierikovadominika@gmail.com>
2020-10-21 19:14:21 +0200

    skoro

--- menu.php
+++ menu.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Uvod</title>
+    <title>Menu</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -12,24 +12,39 @@
     <link href="https://fonts.googleapis.com/css2?family=Sansita+Swashed:wght@800&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Tenor+Sans&amp;display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="uvodna_strana.css">
+    <link rel="stylesheet" href="hlavne.css">
 </head>
 <body>
 
 <!--footer-->
-<!--<div class="footer">-->
-<!--    <p>Dominika Web Design, Copyright &copy; 2020</p>-->
-<!--</div>-->
+<div class="footer">
+    <p>Dominika Web Design, Copyright &copy; 2020</p>
+</div>
 
-<img src="obloha.jpg" class="img-fluid" alt="Responsive image">
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="obrazkyUvod/chmel.jpg" alt="budova podniku">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="obrazkyUvod/slide-01.jpg" alt="vnútro podniku">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="obrazkyUvod/slide-03.jpg" alt="výčap spredu">
+        </div>
+    </div>
+</div>
 
 <!--menu-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top ">
-    <a class="navbar-brand" href="uvodna_strana.html">Piváreň u nás</a>
+    <a class="navbar-brand" href="#">
+        <img src="logo.png" width="50" height="50" alt="" loading="lazy">
+    </a>
+    <a class="navbar-brand nazov" href="uvodna_strana.html">Piváreň u nás</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse stranky" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
                 <a class="nav-link" href="uvodna_strana.html">O nás</a>
@@ -61,7 +76,7 @@
             $capujeme = fopen("capujeme.txt", "r") or die("Unable to open file!");
             $piva = fopen("piva.txt", "r") or die("Unable to open file!");
             $obrazky = fopen("obrazky.txt", "r") or die("Unable to open file!");
-            while(!feof($capujeme)) {
+            while(!feof($capujeme) && !feof($obrazky) && !feof($piva)) {
         ?>
         <div class="row">
             <img src="<?php echo fgets($obrazky)?>" alt="..." class="rounded mx-auto d-block img-fluid">
